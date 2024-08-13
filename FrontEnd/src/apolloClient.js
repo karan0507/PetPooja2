32ff3de5ccb61e4cf799c884a74b298c35d518a3
karang <karangandhi0705@gamil.com>
2024-08-13 10:18:00 -0400

    apoloclient setup

--- FrontEnd/src/apolloClient.js
+++ FrontEnd/src/apolloClient.js
@@ -8,10 +8,22 @@
 
 // export default client;
 
+
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+const getBackendUrl = () => {
+  // Check if the app is running in production or locally
+  const isLocalhost = window.location.hostname === "localhost";
+
+  if (isLocalhost) {
+    return 'http://localhost:5000/graphql';
+  } else {
+    return process.env.REACT_APP_BACKEND_URL || 'https://petpooja2.onrender.com/graphql';
+  }
+};
+
 const client = new ApolloClient({
- link: new HttpLink({ uri: 'http://localhost:5000/graphql' || 'https://petpooja2.onrender.com/graphql'  }),
+  link: new HttpLink({ uri: getBackendUrl() }),
   cache: new InMemoryCache(),
 });
 
