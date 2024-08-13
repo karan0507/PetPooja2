100aaaa74ceee63c001e84e769ee6e7244997428
karang <karangandhi0705@gamil.com>
2024-08-13 00:58:40 -0400

    backend hosting setup

--- FrontEnd/src/apolloClient.js
+++ FrontEnd/src/apolloClient.js
@@ -1,8 +1,19 @@
 // src/apolloClient.js
+// import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+// const client = new ApolloClient({
+//   link: new HttpLink({ uri: 'http://localhost:5000/graphql' }),
+//   cache: new InMemoryCache(),
+// });
+
+// export default client;
+
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:5000/graphql' }),
+  link: new HttpLink({ 
+    uri: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/graphql' 
+  }),
   cache: new InMemoryCache(),
 });
 
